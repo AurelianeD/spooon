18536a81682474c17fbb441ad6612dbd4454c3b8
Auréliane DOR <aureliane.dor@gmail.com>
2023-03-29 16:20:25 +0200

    header + style

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,10 +1,18 @@
 <script>
-	import spoonNumber from '../stores';
 	import "../app.css";
 </script>
 
-<div>
-		<a href="/">Accueil</a>
+<div class="flex flex-row p-5 justify-between">
+	<a href="/">
+		<img src="src/lib/assets/logo.png" alt="logo" class="w-32" />
+	</a>
+	<a href="help">
+		<div>
+			<p class="underline">Aide</p>
+		</div>
+	</a>
+</div>
+<div class="px-5">
 	<slot />
 </div>
 
