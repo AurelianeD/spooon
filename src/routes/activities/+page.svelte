7a991e8250237ef33819fb26fc16e8426b923efe
Auréliane DOR <aureliane.dor@gmail.com>
2023-03-29 16:20:51 +0200

    activity + help pages

--- src/routes/activities/+page.svelte
+++ src/routes/activities/+page.svelte
@@ -1,6 +1,28 @@
 <script lang="ts">
 	import spoonNumber from '../../stores';
-	let spoons = [1, 2, 3, 4];
+	let spoons = [
+		{
+			number: 1,
+			text: 'Activité peu fatiguante'
+		},
+		{
+			number: 2,
+			text: 'Activité fatiguante'
+		},
+		{
+			number: 3,
+			text: 'Activité très fatiguante'
+		},
+		{
+			number: 4,
+			text: 'Activité épuisante'
+		}
+	];
+	let spoonSelected: number;
+
+	function onSelectSpoon(spoon: number){
+		spoonSelected = spoon;
+	};
 
 	function onPressSpoon(spoon: number){
 		spoonNumber.update(n => n - spoon);
@@ -8,12 +30,19 @@
 </script>
 
 <div>
-	<h1>Activité</h1>
-	<div class="flex flex-row">
+	<h1>Combien de cuillière avez vous dépensez ?</h1>
+	<div class="flex flex-col gap-6">
 		{#each spoons as spoon}
-			<div class="bg-slate-500	border-sm">
-				<button on:click={() => onPressSpoon(spoon)}><a href="/">{spoon} cuillière</a></button>
-			</div>
+			<button class="bg-slate-500	border-sm p-10" on:click={() => onSelectSpoon(spoon.number)}>
+				<div>
+					<p>{spoon.number}</p>
+					<p>{spoon.text}</p>
+				</div>
+			</button>
 		{/each}
 	</div>
+
+	<button on:click={() => onPressSpoon(spoonSelected)} class="border-black border-2 p-2 mt-10 mx-auto w-full">
+		<a href="/">Valider</a>
+	</button>
 </div>
---
+++ src/routes/help/+page.svelte
@@ -0,0 +1,11 @@
+<script lang="ts"></script>
+
+<div>
+	<h1>Gérez votre niveau de fatigue au quotidien. </h1>
+	<div class="flex flex-col gap-4">
+		<p>Spooon c’est l’application qui vous permet de mieux gérer votre réservoir d’énergie.</p>
+		<p>Basée sur la théorie des cuillères de Christine Miserandino, Spooon vous accompagne tout au long de vos journées.</p>
+		<p>Le principe est simple, à chaque activité effectuée dans votre journée, dites à Spooon combien d’énergie vous avez dépensé en sélectionnant 1, 2, 3, ou 4 cuillères. 1 cuillère correspond à une unité d’énergie.</p>
+		<p>Suivez-donc en temps réel votre taux de fatigue, afin de pouvoir anticiper l’organisation de vos activités quotidiennes.</p>
+	</div>
+</div>
