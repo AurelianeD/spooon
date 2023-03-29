58156537d1dc814728f908d2b941106ad10b2981
Maxime <lust.maxime@outlook.fr>
2023-03-29 12:39:44 +0200

    Auto stash before merge of "main" and "origin/main"

---
+++ src/components/AddActivities.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+    export let activityName:string;
+    export let getValue:()=>void;
+</script>
+
+<div>
+    <input type="text" id="activity" bind:value={activityName}>
+    <button on:click={() => getValue()}>Ajouter une activité</button>
+</div>
