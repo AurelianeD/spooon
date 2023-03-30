d2503fbfdd2183ec10dc008c480265a44355fec3
Maxime <lust.maxime@outlook.fr>
2023-03-30 15:34:36 +0200

    fix: help page

--- src/routes/help/+page.svelte
+++ src/routes/help/+page.svelte
@@ -1,10 +1,11 @@
 <script lang="ts"></script>
 
 <div>
-	<h1>Gérez votre niveau de fatigue au quotidien. </h1>
-	<div class="flex flex-col gap-4">
+	<h1 class="text-2xl font-semibold">Gérez votre niveau de fatigue au quotidien. </h1>
+	<div class="flex flex-col gap-4 items-center">
 		<p>Spooon c’est l’application qui vous permet de mieux gérer votre réservoir d’énergie.</p>
-		<p>Basée sur la théorie des cuillères de Christine Miserandino, Spooon vous accompagne tout au long de vos journées.</p>
+		<p>Basée sur <a href="https://butyoudontlooksick.com/articles/written-by-christine/the-spoon-theory/" class="underline">la théorie des cuillères de Christine Miserandino</a>, Spooon vous accompagne tout au long de vos journées.</p>
+		<img src="src/lib/assets/cuilleres.png" alt="" class="h-24 w-24">
 		<p>Le principe est simple, à chaque activité effectuée dans votre journée, dites à Spooon combien d’énergie vous avez dépensé en sélectionnant 1, 2, 3, ou 4 cuillères. 1 cuillère correspond à une unité d’énergie.</p>
 		<p>Suivez-donc en temps réel votre taux de fatigue, afin de pouvoir anticiper l’organisation de vos activités quotidiennes.</p>
 	</div>
