34dbd15ba1c272bf7801dfa0f03ad73dd9b6bc58
Maxime <lust.maxime@outlook.fr>
2023-03-30 11:46:12 +0200

    fix: text-center home page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -15,7 +15,7 @@
 		<img src="src/lib/assets/cuilleres.png" alt="">
 		<p class="text-4xl absolute">{$spoonNumber}</p>
 	</div>
-	<h1 class="text-2xl font-bold mx-auto flex items-center justify-center">{handleTextSpoon($spoonNumber)}</h1>
+	<h1 class="text-2xl font-bold mx-auto flex items-center justify-center text-center">{handleTextSpoon($spoonNumber)}</h1>
 	<button class="mx-auto flex items-center justify-center">
 		<div class="h-12 w-80 rounded-md bg-gradient-to-b from-darkBlue to-lightBlue p-[2px]">
 			<div class="flex items-center justify-center h-full w-full bg-white rounded">
