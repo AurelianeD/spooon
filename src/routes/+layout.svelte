833deb9c25241df5cf3d83d807af3501773605b3
Auréliane DOR <aureliane.dor@gmail.com>
2023-03-31 12:20:24 +0200

    fix header + add favicon

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -7,18 +7,10 @@
 </script>
 <!--//TODO: align bottom-->
 <div class="flex flex-row p-5 justify-between items-center mb-10">
-	{#if $page.url.pathname !== '/'}
-		<a href="/" class="flex flex-row items-center gap-2">
-			<img src={favicon} alt="logo" class="w-8"/>
-			<p class="underline text-2xl">Retour</p>
-		</a>
-		{:else}
 		<a href="/">
 			<img src={logo} alt="logo" class="w-32"/>
 		</a>
-	{/if}
-	{#if $page.url.pathname !== '/help'}
-		<a href="help">
+		<a href="https://www.spooon.org">
 			<div class="flex flex-row gap-2 text-[18px] items-center">
 				<p class="font-century text-xl text-center">À propos</p>
 				<div class="border-2 border-lightBlue rounded-full w-8 flex justify-center">
@@ -26,7 +18,6 @@
 				</div>
 			</div>
 		</a>
-	{/if}
 </div>
 <div class="px-5">
 	<slot />
