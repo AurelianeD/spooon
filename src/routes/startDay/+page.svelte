f8c4444e37efed59e6c0b08524c01de52dc7cda1
Auréliane DOR <aureliane.dor@gmail.com>
2023-04-12 17:42:36 +0200

    fix responsive

--- src/routes/startDay/+page.svelte
+++ src/routes/startDay/+page.svelte
@@ -5,9 +5,9 @@
 
 	let src = '$lib/assets/logo.png';
 	let energies = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
-	let energySelected = 1;
+	let energySelected = 12;
 
-	let energiesContainerStyle = "rounded-md flex flex-row border-2 border-lightBlue bg-gray gap-6 overflow-x-scroll px-32 py-5 no-scrollbar";
+	let energiesContainerStyle = "rounded-md flex flex-row border-2 border-lightBlue bg-gray gap-6 overflow-x-scroll px-32 py-3 no-scrollbar";
 	let energySelectedStyle = "border border-darkBlue rounded-full px-4 py-2";
 
 	function onSelectEnergy(energy: number) {
@@ -22,18 +22,20 @@
 
 </script>
 
-<div class="">
-	<h1 class="text-center pb-10">Comment vous sentez-vous aujourd'hui ?</h1>
-	<h2 class="pb-5">Votre quantité d'energie sur une échelle de 12</h2>
-	<p class="text-darkBlue pb-5 text-center">{handleStartDayText(energySelected)}</p>
-	<div class={energiesContainerStyle}>
-		{#each energies as energy}
-				<div class={energy === energySelected ? energySelectedStyle : "p-2"}>
-					<button on:click={() => onSelectEnergy(energy)} class="text-darkBlue text-xl">
-						{energy}
-					</button>
-				</div>
-		{/each}
+<div class="h-full">
+	<h1 class="text-center">Comment vous sentez-vous aujourd'hui ?</h1>
+	<div class="h-[80%] flex flex-col justify-center">
+		<h2 class="pb-10">Votre quantité d'energie sur une échelle de 12</h2>
+		<h2 class="text-darkBlue text-center pb-3">{handleStartDayText(energySelected)}</h2>
+		<div class={energiesContainerStyle}>
+			{#each energies as energy}
+					<div class={energy === energySelected ? energySelectedStyle : "p-2"}>
+						<button on:click={() => onSelectEnergy(energy)} class="text-darkBlue text-xl">
+							{energy}
+						</button>
+					</div>
+			{/each}
+		</div>
 	</div>
 	<Button title="Démarrer la journée" onPress={() => {onBeginDay()}} href="/"/>
 </div>
